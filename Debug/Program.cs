99df8831cc72e31dba8b38f926c26231ffe0ac73
mathe-man <mathieumousset6@gmail.com>
2025-07-18 20:56:33 +0300

    Add different entry for debug groups

--- Debug/Program.cs
+++ Debug/Program.cs
@@ -4,16 +4,24 @@ using MavraLib;
 
 class Program
 {
+	private static Body a = new(500E6f, new(0, 0))
+		{ Name = "A Body" };
+		
+	private static Body b = new (150E6f, new (5, 1), 1, new (0, 0.07f))
+		{ Name = "B Body" };
+	
+	
+	private static Universe universe = new(6.67408E-11f, [a, b]);
+	private static EvolutiveUniverse evo = new(universe);
+	
 	static void Main(string[] args)
 	{
-		Body a = new (500E6f, new (0, 0));
-		a.Name = "A";
-		Body b = new (150E6f, new (5, 1), 1, new (0, 0.07f));
-		b.Name = "B";
-		
-		Universe seed = new(6.67408E-11f, [a, b]);
-		EvolutiveUniverse evo = new(seed);
-		evo.ComputeEvolution(10000);
+		DebugEvolution(args);
+	}
+
+	static void DebugEvolution(string[]? args = null)
+	{
+		evo.ComputeEvolution(10000000);
 		
 		evo.EnterConsoleInspection();
 	}
